ecad1bd7f6a30bc128cd508351081f8a209470b0
yakiimokko <ykm.rad.creep.s11@gmail.com>
2021-12-20 21:09:25 +0000

    downcase test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  before_save {self.email = email.downcase}
+  before_save {email.downcase!} #右辺にある場合selfは省略できるから多分省略した
+  #self.email = email.downcaseをやるために!をつける（代入する作業を省略した）
+  #self.email.downcaseだけだとdowncaseを出力しただけで、self.emailは元の大文字のままになる
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
