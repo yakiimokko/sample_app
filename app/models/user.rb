bf8ef6c2a95bf9aaa563d274b01827c533fb51ad
yakiimokko <ykm.rad.creep.s11@gmail.com>
2022-01-03 23:25:59 +0000

    final

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
                     uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  #has_secure_passwordのvalidateに存在性含まれるが更新したときに適用されないためこっちでもpresenceする
 end
